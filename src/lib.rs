db90873a620ef3fc6717fc6444be011fd68dd2a7
laitsky <vincent.diamond15@gmail.com>
2022-10-15 16:10:23 +0700

    add pg docker config files

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 use std::net::TcpListener;
-use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+use actix_web::{get, post, App, HttpServer, Responder, HttpResponse, web};
 use actix_web::dev::Server;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[post("/subscriptions")]
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
     HttpResponse::Ok()
 }
 
@@ -11,6 +22,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .service(health_check)
+            .service(subscribe)
     })
         .listen(listener)?
         .run();
